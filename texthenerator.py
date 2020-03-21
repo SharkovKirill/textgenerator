648e2374cad7589bbe43fb7cfac4de3f5c8cce76
SharkovKirill <kirill.office75@gmail.com>
2020-03-21 11:07:11 +0700

    finish

--- texthenerator.py
+++ texthenerator.py
@@ -1,22 +1,24 @@
 """Case-study #6 Генерация предложений
 Разработчики:
-Шарков К., Кеда С., Ермоленко В.
+Шарков К.(90%), , Ермоленко В.
 """
 import random
 
 cor_symb = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
             'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '0', '1', '2', '3', '4', '5', '6', '7', '8',
-            '9', '.', ',', '?', '!']
+            '9', '.', ',', '?', '!', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'l', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
+filename = input('Введите название файла(файл должен быть в ANSI, программа может работать с английским текстом):')
+n = int(input('Введите количество желаемых предложений:'))
 lines = []
-with open('ansibig.txt') as dict_file:
+with open(filename) as dict_file:
     first_lst = dict_file.read()
 lines = first_lst.split()
 new = []
 for word in lines:
     for symbol in word:
-        if symbol.lower() not in cor_symb:                # getting a list of consecutive words
+        if symbol.lower() not in cor_symb:  # getting a list of consecutive words
             word = word[:word.find(str(symbol))] + word[1 + word.find(str(symbol)):]
     new.append(word)
 while True:
@@ -24,29 +26,56 @@ while True:
         new.remove('')
     except ValueError:
         break
-print(len(new), new)
-
 
 start_words = []
 for word in new:
     if word.istitle():
-        start_words.append(word)      # list of capitalized words
-print(len(start_words),start_words)
+        start_words.append(word)  # list of capitalized words
 
-spsp=[]
-for word in range(len(new)-1):
+spsp = []
+for word in range(len(new) - 1):
     chet = 0
     for i in range(len(spsp)):
         if new[word] == spsp[i][0]:
-            if new[word+1] not in spsp[i]:
-                spsp[i].append(new[word+1])  # spsp - list of lists, the zero word of each sublist is the key
+            if new[word + 1] not in spsp[i] and not new[word + 1].istitle():
+                spsp[i].append(new[word + 1])  # spsp - list of lists, the zero word of each sublist is the key
             chet = 1
-    if chet == 0 and not(new[word].endswith('.') or new[word].endswith('!') or new[word].endswith('?')):
-        spsp.append([new[word],new[word+1]])
-print(len(spsp), spsp)
-
-# TODO:(Vova): def that generates a random sentence. The first word of each sentence is taken from start_words.
-
-# TODO:(Sveta): PEP-8, output a certain number of sentences, adaptation for english text.
+    if chet == 0 and not (new[word].endswith('.') or new[word].endswith('!') or new[word].endswith('?')):
+        spsp.append([new[word], new[word + 1]])
 
 
+def gener(start_words, spsp):
+    predloj = []
+    st = start_words[random.randrange(0, len(start_words), 1)]
+    predloj.append(st)
+
+    for all in range(len(spsp)):
+        if spsp[all][0] == st:
+            rand = random.randrange(1, len(spsp[all]), 1)
+            word = spsp[all][rand]
+            predloj.append(word)
+
+    while not (predloj[len(predloj) - 1].endswith('.') or predloj[len(predloj) - 1].endswith('!') or
+               predloj[len(predloj) - 1].endswith('?')) and len(predloj) < 19:
+        for all in range(len(spsp)):
+            if spsp[all][0] == word:
+                rand = random.randrange(1, len(spsp[all]), 1)
+                newword = spsp[all][rand]
+                predloj.append(newword)
+        word = newword
+
+    if not (predloj[len(predloj) - 1].endswith('.') or predloj[len(predloj) - 1].endswith('!') or
+            predloj[len(predloj) - 1].endswith('?')):
+        lastword = str(predloj.pop) + '.'
+        predloj.append(lastword)
+
+    if 4 < len(predloj) < 20:
+        return predloj
+    else:
+        return gener(start_words, spsp)
+
+def main():
+    for i in range(n):
+        print(' '.join(gener(start_words, spsp)), end=' ')
+
+main()
