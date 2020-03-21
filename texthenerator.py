4964678a6334318e51333a33f625e197c56d3a07
Vladimir-Ermolenko <v.ermolenko@g.nsu.ru>
2020-03-21 12:55:10 +0700

    comms + pep

--- texthenerator.py
+++ texthenerator.py
@@ -1,37 +1,49 @@
 """Case-study #6 Генерация предложений
 Разработчики:
-Шарков К.(95%), , Ермоленко В.
+Шарков К.(95%), , Ермоленко В. (10%)
 """
+
+# importing the module that gives an opportunity to use a randomized value
 import random
 
+# the list of all the symbles
 cor_symb = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у',
             'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '0', '1', '2', '3', '4', '5', '6', '7', '8',
             '9', '.', ',', '?', '!', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'l', 'n', 'o', 'p',
             'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+# getting all the needed info from the user
 filename = input('Введите название файла(файл должен быть в ANSI, программа может работать с английским текстом):')
 n = int(input('Введите количество желаемых предложений:'))
 lines = []
+
+# getting a list of strings in a file
 with open(filename) as dict_file:
     first_lst = dict_file.read()
 lines = first_lst.split()
 new = []
+
+# getting a list of consecutive words
 for word in lines:
     for symbol in word:
-        if symbol.lower() not in cor_symb:  # getting a list of consecutive words
+        if symbol.lower() not in cor_symb:
             word = word[:word.find(str(symbol))] + word[1 + word.find(str(symbol)):]
     new.append(word)
+
+# removing unneeded symbols
 while True:
     try:
         new.remove('')
     except ValueError:
         break
 
+# getting a list of capitalized words
 start_words = []
 for word in new:
     if word.istitle():
-        start_words.append(word)  # list of capitalized words
+        start_words.append(word)
 
+# making a list that contents the keys
 spsp = []
 for word in range(len(new) - 1):
     chet = 0
@@ -44,6 +56,7 @@ for word in range(len(new) - 1):
         spsp.append([new[word], new[word + 1]])
 
 
+# the main function that generates the text itsels
 def gener(start_words, spsp):
     predloj = []
     st = start_words[random.randrange(0, len(start_words), 1)]
